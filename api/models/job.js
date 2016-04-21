1aaa52ed27a33661abb6919c85ddb89a868a1cf6
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-04-21 18:37:18 +0200

    Definición de modelo job

--- api/models/job.js
+++ api/models/job.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var jobSchema = new Schema({
-    
+    name: String,
+    description: String,
+    enterprise: String,
+    urlLogo: String,
+    yearInit: Number,
+    yearEnd: Number,
+    active: Boolean
 });
 
 module.exports = mongoose.model('Job', jobSchema);
