7af4265e8999dfd33aba1629c6be3df4d269b1c8
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-04-27 22:46:45 +0200

    Quitar conexión

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ module.exports = {
         port: process.env.PORT || 4001
     },
     mongodb: {
-        url: 'mongodb://jdonsan:mycv2016@ds013941.mlab.com:13941/my-cv'
+        url: ''
     },
     resources: [
         'course',
