557ff1ec659afb8a788787fa5825688dbca97e2f
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-04-30 00:27:06 +0200

    Arreglo bug en tarea de copiar fuentes

--- gulp/copy.js
+++ gulp/copy.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 module.exports = function () {
-    return gulp.src('./front/fonts/*').pipe(gulp.dest('./statics/fonts'));
+    return gulp.src('./front/fonts/**/*').pipe(gulp.dest('./statics/fonts'));
 };
 
