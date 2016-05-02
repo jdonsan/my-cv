4d31cc637e83d97bd8e9617de93cc68b571487b3
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-05-02 21:22:38 +0200

    Mejoras en componentes

--- front/src/core/components/index.js
+++ front/src/core/components/index.js
@@ -1,9 +1,11 @@
 import header from './toolbar';
 import drawer from './drawer';
 import main from './main';
+import jumbotron from './jumbotron';
 
 angular
     .module('app.core.components', [])
     .component('jdsToolbar', header)
     .component('jdsDrawer', drawer)
-    .component('jdsMain', main);
+    .component('jdsMain', main)
+    .component('jdsJumbotron', jumbotron);
