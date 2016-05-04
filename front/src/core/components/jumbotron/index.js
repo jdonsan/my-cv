1eef4f5bc0272190c8be3c787fc7ee825b154329
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-05-05 00:17:27 +0200

    jumbotron con texto dinamico y cambio a vistas parciales

--- front/src/core/components/jumbotron/index.js
+++ front/src/core/components/jumbotron/index.js
@@ -1,3 +1,6 @@
 export default {
-    templateUrl: 'src/core/components/jumbotron/template.html'
+    templateUrl: 'src/core/components/jumbotron/template.html',
+    bindings: {
+        tittle: '@'
+    }
 };
