2957d7a7193ad3b318a085a274c3bde4085615e2
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-05-11 20:30:45 +0200

    Inclusión de imagenes y datos reales en el apartado de estudios

--- data/study.js
+++ data/study.js
@@ -2,30 +2,40 @@ module.exports = {
     model: 'Study',
     documents: [
         {
-            name: 'Ingeniería Informática',
-            description: 'Ingeniero superios',
+            name: 'Ingeniería Superior en Informática',
+            description: `
+                Las bases de mi conocimiento, las aprendí en la Politécnica de Alcalá. 
+                Disfrute mucho y aprendí de grandes profesionales. Gracias a asignaturas como 
+                'Desarrollo de aplicaciones Web', descubrí mi vocación.
+            `,
             academy: 'Universidad Alcala de Henares',
-            yearInit: 2009,
-            yearEnd: 2012,
-            urlLogo: 'img/logo/uah.jpg',
+            yearInit: 2006,
+            yearEnd: 2011,
+            urlLogo: 'img/study/uah.jpg',
             active: true
         },
         {
-            name: 'Ingeniería Informática',
-            description: 'Ingeniero superios',
-            academy: 'Universidad Alcala de Henares',
-            yearInit: 2009,
-            yearEnd: 2012,
-            urlLogo: 'img/logo/uah.jpg',
+            name: 'Preparación para CERTIFICACION MICROSOFT- 3 MCTS (Windows - WEB)',
+            description: `
+                Siempre sentí curiosidad por adentrarme en las tecnologías de Microsoft y 
+                este Master me ayudó como punto de en mi trayectoria dentro del mundo .NET
+            `,
+            academy: 'Academia AZPE',
+            yearInit: 2012,
+            yearEnd: 2013,
+            urlLogo: 'img/study/azpe.jpg',
             active: true
         },
         {
-            name: 'Ingeniería Informática',
-            description: 'Ingeniero superios',
-            academy: 'Universidad Alcala de Henares',
-            yearInit: 2009,
-            yearEnd: 2012,
-            urlLogo: 'img/logo/uah.jpg',
+            name: 'Máster en Ingeniería de Software y Sistemas Informáticos',
+            description: `
+                Nunca quise dejar de lado la formación académica y científica que me aportaba la universidad,
+                por eso decidí realizar un Master que aumentase mis conocimientos en Ingeniería y Metodologías
+            `,
+            academy: 'UNED',
+            yearInit: 2014,
+            yearEnd: 2016,
+            urlLogo: 'img/study/uned.jpg',
             active: true
         }
     ]
