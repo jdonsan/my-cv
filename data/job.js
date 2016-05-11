fce13eb45e1e5d0e18b9db10924505fa9cbbfeec
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-05-11 20:33:03 +0200

    Mejoras sobre la funcionalidad job

--- data/job.js
+++ data/job.js
@@ -3,9 +3,13 @@ module.exports = {
     documents: [
         {
             name: 'Desarrollador Front',
-            description: 'Desarollador de aplicaciones front y móviles',
+            description: `
+                He realizado tantas cosas en Altran que me son difíciles de enumerar. Por eso solo me voy a quedar
+                con lo que hago actualmente: me dedico a desarrollar soluciones digitales para diferentes empresas de los 
+                sectores banca y seguros.
+            `,
             enterprise: 'Altran Group',
-            urlLogo: '/img/logo.jpg',
+            urlLogo: '/img/job.jpg',
             yearInit: 2011,
             yearEnd: 2016,
             active: true
