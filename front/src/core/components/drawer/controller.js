b0af32b2ec33d14a0a0dc4485e8aadd2e5d8c8b5
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2016-05-12 19:19:46 +0200

    Cierre del drawer al pulsar y refactor del html extrayendo modelo al ctrl

---
+++ front/src/core/components/drawer/controller.js
@@ -0,0 +1,43 @@
+export default function ($scope) {
+    'ngInject'
+    
+    const dr = this;
+
+    dr.active = false;
+    dr.options = [{
+        link: 'app.home',
+        icon: 'account_circle',
+        text: 'Perfil',
+        active: true
+    }, {
+        link: 'app.skills',
+        icon: 'vpn_key',
+        text: 'Conocimientos',
+        active: false
+    }, {
+        link: 'app.jobs',
+        icon: 'work',
+        text: 'Experiencia',
+        active: false
+    }, {
+        link: 'app.study',
+        icon: 'school',
+        text: 'Estudios',
+        active: false
+    }, {
+        link: 'app.contact',
+        icon: 'rss_feed',
+        text: 'Contacto',
+        active: false
+    }];
+    dr.activeOption = function (index) {
+        dr.options.forEach((option) => option.active = false);
+        dr.options[index].active = true;
+        dr.active = false;
+    };
+    
+    $scope.$on('drawer:toggle', function () {
+        dr.active = !dr.active;
+        $scope.$apply();
+    });
+}
--- front/src/core/components/drawer/index.js
+++ front/src/core/components/drawer/index.js
@@ -1,14 +1,7 @@
+import DrawerController from './controller';
+
 export default {
     templateUrl: 'src/core/components/drawer/template.html',
-    controller: function ($scope) {
-        'ngInject'
-        const dr = this;
-        
-        dr.active = false;
-
-        $scope.$on('drawer:toggle', function () {
-            dr.active = !dr.active;
-            $scope.$apply();
-        });
-    }
+    controller: DrawerController,
+    controllerAs: 'dr'
 };
